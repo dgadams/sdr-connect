279d0efd1cc6369cc15bc3b45d8f762600cab856
dgadams <dgadams.nm@gmail.com>
2025-03-16 00:20:01 -0600

    Update play-muntz.sh
    Shorten some command lines in the scrpt.

--- play-muntz.sh
+++ play-muntz.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script uses a feature of bash - extglob which allows rm !(exception_list) to
-# selectively remove any file in the current image layer except those listed.  
+# selectively remove any file in the current image layer except those listed.
 # Used for muntzing libraries and files down to the minimal set needed to run the application.
 
 shopt -s extglob
@@ -21,13 +21,11 @@ shopt -s extglob
 	EXC+=")"
 	rm -fr $EXC
 
-#   Nuke some misc stuff, /usr/sbin and /usr/bin
-    rm -rf /var/lib/dpkg
-    rm -rf /var/lib/apt
-    rm -rf /var/cache/debconf
-	rm -rf /var/cache/apt
-    rm -rf /usr/share
-    rm -rf /usr/sbin/*
+#   Nuke anything not needed in the container
+    cd /var && rm -rf !(nothing)
+    cd /etc && rm -rf apt dpkg
+    cd /usr && rm -rf !(lib|bin|sbin|lib64|libexec)
 
-    cd /usr/bin
-    rm !(busybox)
+#   Now clean out sbin and bin
+    cd /usr/sbin && rm -rf !(nothing)
+    cd /usr/bin  && rm -rf !(busybox)
